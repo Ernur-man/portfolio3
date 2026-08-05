52016f6eb2e1666c5ad4b5de42586bb26a52240c
Ernur-man <ernuraman926@gmail.com>
2026-08-05 11:37:06 +0500

    feat: add new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Experience from './component/experince'
 import Message from './component/message'
+import AboutPage from './Pages/AboutPage'
 import ContactPage from './Pages/ContactPage'
 import HomePage from './Pages/HomePage'
 import ProjectsPage from './Pages/ProjectsPage'
@@ -13,6 +14,7 @@ function App() {
     <>
       <Message/>
       <HomePage/>
+      <AboutPage/>
       <Experience/>
       <ProjectsPage/>
       <WorkPage/>
